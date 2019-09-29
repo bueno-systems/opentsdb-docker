80b794677083f9719657f1c05fcebdd98ca24b4d
Richard Harrison <rharrison@buenosystems.com.au>
2019-09-30 08:07:28 +1000

    fix: add valid ttl for tsdb hbase table

--- files/create_tsdb_tables.sh
+++ files/create_tsdb_tables.sh
@@ -4,6 +4,7 @@ export COMPRESSION="NONE"
 export HBASE_HOME=/opt/hbase
 export TSDB_VERSION="::TSDB_VERSION::"
 export JAVA_HOME="::JAVA_HOME::"
+export TSDB_TTL=2147483647
 
 /usr/local/share/opentsdb/tools/create_table.sh
 touch /data/hbase/opentsdb_tables_created.txt
